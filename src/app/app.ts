a7ae1d782a26e5f465d220c7b004ffe96637f9e2
R4r4s <rngheru@gmail.com>
2026-01-22 10:01:31 +0100

    Instalar GSAP

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('tienda-cliente-fronted');
+
+  ngAfterViewInit(): void {
+    gsap.from('.content', { opacity: 0, y: 12, duration: 0.6, ease: 'power2.out' });
+  }
 }
