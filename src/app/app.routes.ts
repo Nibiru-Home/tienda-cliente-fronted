5075d512d3002f446ef5e4bd154941dd99d87997
R4r4s <rngheru@gmail.com>
2026-01-26 19:28:28 +0100

    feat(index-page): reorganizar estructura de archivos y restaurar componentes de la página de índice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { IndexPage } from './components/pages/index';
+import { IndexPage } from './components/pages/index/index';
 
 export const routes: Routes = [
   { path: '', component: IndexPage },
