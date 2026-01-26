8da7ae86410cd3a5b596acefed337ddcd4665161
R4r4s <rngheru@gmail.com>
2026-01-26 18:20:09 +0100

    feat(index-cards): agregar componente de tarjetas de inspiración y eliminar sección de inspiración del índice

--- src/app/components/pages/index.ts
+++ src/app/components/pages/index.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BentoGalleryComponent } from '../../transitions/features/components/bento-gallery/bento-gallery.component';
+import { IndexCardsComponent } from '../ui/index-cards/index-cards.component';
 
 @Component({
   selector: 'app-index-page',
   standalone: true,
-  imports: [BentoGalleryComponent],
+  imports: [BentoGalleryComponent, IndexCardsComponent],
   templateUrl: './index.html',
   styleUrl: './index.scss'
 })
---
+++ src/app/components/ui/index-cards/index-cards.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-cards',
+  standalone: true,
+  templateUrl: './index-cards.component.html',
+  styleUrl: './index-cards.component.scss'
+})
+export class IndexCardsComponent {}
