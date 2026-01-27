3afb1f917881424972f45fb2e542d01c12beabcf
ainhoa <ainhoamiret@gmail.com>
2026-01-27 20:00:13 +0100

    feat(Nibiru-Home/sprints#11): Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { IndexPage } from './components/pages/index/index';
 import { ProductListComponent } from './components/pages/product-list/product-list';
 import { Login } from './components/pages/login/login';
 import { ProductDetailPage } from './components/pages/product-detail/product-detail';
+import { Register } from './components/pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: IndexPage },
   { path: 'products', component: ProductListComponent },
   { path: 'login', component: Login },
+  { path: 'register', component: Register },
   { path: 'product', component: ProductDetailPage },
   { path: '**', redirectTo: '' }
 ];
