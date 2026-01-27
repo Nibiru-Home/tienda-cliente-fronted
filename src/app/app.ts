a9b16ba7b2588d234e49cf115a8552a02b5840dd
R4r4s <rngheru@gmail.com>
2026-01-27 08:53:57 +0100

    feat: reestructurar componentes de la aplicación y actualizar importaciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutHeaderComponent } from './components/layout/header/header.component';
-import { LayoutFooterComponent } from './components/layout/footer/footer.component';
+import { LayoutHeaderComponent } from './components/layout/header/header';
+import { LayoutFooterComponent } from './components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
--- src/app/components/pages/product-list/product-list.ts
+++ src/app/components/pages/product-list/product-list.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../../models/product.model';
 import { ProductService } from '../../../services/product.service';
-import { ProductCardComponent } from '../../ui/product-card/product-card.component';
+import { ProductCardComponent } from '../../ui/product-card/product-card';
 
 @Component({
     selector: 'app-product-list',
