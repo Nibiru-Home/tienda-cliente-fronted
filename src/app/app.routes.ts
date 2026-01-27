8a6813942578af39dbea5dbd8d1f72c97d068da2
R4r4s <rngheru@gmail.com>
2026-01-27 16:58:45 +0100

    fix(routes): agregar rutas para Login y ProductDetailPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { IndexPage } from './components/pages/index/index';
 import { ProductListComponent } from './components/pages/product-list/product-list';
+import { Login } from './components/pages/login/login';
+import { ProductDetailPage } from './components/pages/product-detail/product-detail';
 
 export const routes: Routes = [
   { path: '', component: IndexPage },
